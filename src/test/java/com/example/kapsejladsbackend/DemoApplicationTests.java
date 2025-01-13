2e6136ce95472f31d377f3c01e1dd072a9572835
DekaSkole <eser0004@stud.kea.dk>
2025-01-13 16:47:27 +0100

    first commit

---
+++ src/test/java/com/example/kapsejladsbackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kapsejladsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
